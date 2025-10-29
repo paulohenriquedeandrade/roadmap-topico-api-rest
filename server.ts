323ced30eaacf29aaa6c915ae8a194ccc1ebffd7
Paulo Henrique <paulo.henrique@ecenture.com.br>
2025-10-29 11:36:08 -0300

    feat: cria utilitários JWT e configura varáveis de ambiente

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
-import app from './src/app.js';
+import "dotenv/config";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta http://localhost:${PORT}/`);
+  console.log(`Servidor rodando na porta http://localhost:${PORT}/`);
 });
