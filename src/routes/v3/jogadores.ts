07d7204bc04a63c1e0258cc6c47488dddc3f87d4
Paulo Henrique <paulo.henrique@ecenture.com.br>
2025-10-29 14:22:55 -0300

    feat: protege rotas de seleção e jogadores com autenticação JWT

--- src/routes/v3/jogadores.ts
+++ src/routes/v3/jogadores.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import * as jogadorController from "../../controllers/jogadorController.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const jogadoresV3: Router = Router();
 
-jogadoresV3.get("/", jogadorController.listarTodos);
-jogadoresV3.get("/:id", jogadorController.buscarPorId);
-jogadoresV3.post("/", jogadorController.criar);
-jogadoresV3.put("/:id", jogadorController.atualizar);
-jogadoresV3.delete("/:id", jogadorController.deletar);
+jogadoresV3.get("/", authMiddleware, jogadorController.listarTodos);
+jogadoresV3.get("/:id", authMiddleware, jogadorController.buscarPorId);
+jogadoresV3.post("/", authMiddleware, jogadorController.criar);
+jogadoresV3.put("/:id", authMiddleware, jogadorController.atualizar);
+jogadoresV3.delete("/:id", authMiddleware, jogadorController.deletar);
 
 export default jogadoresV3;
--- src/routes/v3/selecoes.ts
+++ src/routes/v3/selecoes.ts
@@ -1,12 +1,13 @@
 import express, { Router } from "express";
 import * as selecaoController from "../../controllers/selecaoController.js";
+import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const selecoesV3: Router = Router();
 
-selecoesV3.get("/", selecaoController.listarTodas);
-selecoesV3.get("/:id", selecaoController.buscarPorId);
-selecoesV3.post("/", selecaoController.criar);
-selecoesV3.put("/:id", selecaoController.atualizar);
-selecoesV3.delete("/:id", selecaoController.deletar);
+selecoesV3.get("/", authMiddleware, selecaoController.listarTodas);
+selecoesV3.get("/:id", authMiddleware, selecaoController.buscarPorId);
+selecoesV3.post("/", authMiddleware, selecaoController.criar);
+selecoesV3.put("/:id", authMiddleware, selecaoController.atualizar);
+selecoesV3.delete("/:id", authMiddleware, selecaoController.deletar);
 
 export default selecoesV3;
