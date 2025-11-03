3b3f03389453e3cf59e9e39d76d75ae3c2afa0b8
Paulo Henrique <paulo.henrique@ecenture.com.br>
2025-11-03 11:20:52 -0300

    docs: adiciona JSDoc no userRepository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,25 +1,60 @@
+/**
+ * Repositório para operações CRUD na entidade "User".
+ * Utiliza o Prisma Client para interagir com o banco de dados.
+ * Fornece métodos para criar, ler e atualizar usuários.
+ * @module repositories/userRepository
+ */
 import { prisma } from "../config/database.js";
 import { User, CreateUser } from "../types/user.types.js";
 
 class UserRepository {
+  /**
+   * Busca um usuário pelo email
+   * @async
+   * @function findByEmail
+   * @param {string} email - Email do usuário
+   * @returns {Promise<User | null>} Usuário encontrado ou null
+   */
   async findByEmail(email: string): Promise<User | null> {
     return prisma.user.findUnique({
       where: { email },
     });
   }
 
+  /**
+   * Busca um usuário pelo ID
+   * @async
+   * @function findById
+   * @param {number} id - ID do usuário
+   * @returns {Promise<User | null>} Usuário encontrado ou null
+   */
   async findById(id: number): Promise<User | null> {
     return prisma.user.findUnique({
       where: { id },
     });
   }
 
+  /**
+   * Cria um novo usuário
+   * @async
+   * @function create
+   * @param {CreateUser} userData - Dados do usuário a ser criado
+   * @returns {Promise<User>} Usuário criado
+   */
   async create(userData: CreateUser): Promise<User> {
     return prisma.user.create({
       data: userData,
     });
   }
 
+  /**
+   * Atualiza o token de refresh do usuário
+   * @async
+   * @function updateRefreshToken
+   * @param {number} id - ID do usuário
+   * @param {string | null} refreshToken - Novo token de refresh ou null para remover
+   * @returns {Promise<User>} Usuário atualizado
+   */
   async updateRefreshToken(
     id: number,
     refreshToken: string | null
