337e2ad99487d3c9a4247e0ef33e9be2768333fe
Paulo Henrique <paulo.henrique@ecenture.com.br>
2025-11-03 10:44:40 -0300

    docs: adiciona JSDoc no selecaoService

--- src/services/selecaoService.ts
+++ src/services/selecaoService.ts
@@ -1,12 +1,34 @@
+/**
+ * Service de Seleções
+ * Contém a lógica de negócio para gerenciar seleções da Copa 2022
+ * @module services/selecaoService
+ */
 import selecaoRepository from "../repositories/selecaoRepository.js";
-import { CreateSelecao, UpdateSelecao } from "../types/selecao.types.js";
+import {
+  CreateSelecao,
+  Selecao,
+  UpdateSelecao,
+} from "../types/selecao.types.js";
 import { NotFoundError } from "../errors/NotFoundError.js";
 
 class SelecaoService {
+  /**
+   * @async
+   * @function listarTodas
+   * @returns {Promise<Selecao[]>} Array com todas as seleções
+   * @throws {Error} Erro ao listar seleções
+   */
   async listarTodas() {
     return await selecaoRepository.findAll();
   }
 
+  /**
+   * @async
+   * @function buscarPorId
+   * @param {number} id - ID da seleção
+   * @returns {Promise<Selecao>} Seleção encontrada
+   * @throws {NotFoundError} Seleção não encontrada
+   */
   async buscarPorId(id: number) {
     const selecao = await selecaoRepository.findById(id);
 
@@ -17,16 +39,37 @@ class SelecaoService {
     return selecao;
   }
 
+  /**
+   * @async
+   * @function criar
+   * @param {CreateSelecao} data - Dados da nova seleção
+   * @returns {Promise<Selecao>} Seleção criada
+   */
   async criar(data: CreateSelecao) {
     return await selecaoRepository.create(data);
   }
 
+  /**
+   * @async
+   * @function atualizar
+   * @param {number} id - ID da seleção a ser atualizada
+   * @param {UpdateSelecao} data - Dados para atualização
+   * @returns {Promise<Selecao>} Seleção atualizada
+   * @throws {NotFoundError} Seleção não encontrada
+   */
   async atualizar(id: number, data: UpdateSelecao) {
     await this.buscarPorId(id);
 
     return await selecaoRepository.update(id, data);
   }
 
+  /**
+   * @async
+   * @function deletar
+   * @param {number} id - ID da seleção a ser deletada
+   * @returns {Promise<void>}
+   * @throws {NotFoundError} Seleção não encontrada
+   */
   async deletar(id: number) {
     await this.buscarPorId(id);
 
